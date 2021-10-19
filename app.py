efc114377dcb489a78c87694ba045aef84da15df
Romain Camisard <romain.camisard@centrale-marseille.fr>
2021-10-19 11:10:59 +0200

    Fix - Get & Insert classement

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 # app.py
 from flask import Flask
+from flask import request
+from flask import Response
+from flask_cors import CORS
 from generateMap import generateMap
 import db
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def hello_world():
@@ -16,16 +21,22 @@ def generate():
     return map
 
 
-@app.route("/insertClassement", methods=['POST'])
+@app.route("/insertClassement", methods=["POST"])
 def insertClassement():
-    username = request.args.get("username")
-    points = request.args.get("points")
-    db.insertClassement(r"./db/sqlite.db", username, points)
-    return 200
+    request_data = request.get_json()
+
+    username = request_data['username']
+    score = request_data['score']
+    print(username)
+    print(score)
+    db.insertClassement(r"./db/sqlite.db", username, score)
+    print(db.getClassements(r"./db/sqlite.db"))
+    return Response("", status=201, mimetype='application/json')
 
 
 @app.after_request
 def add_header(response):
+    response.headers['Access-Control-Allow-Headers'] = '*'
     response.headers['Access-Control-Allow-Origin'] = '*'
     return response
 
--- db.py
+++ db.py
@@ -17,17 +17,17 @@ def getClassements(dbFile):
     dbConnection = sqlite3.connect(dbFile)
     dbCursor = dbConnection.cursor()
 
-    dbCursor.execute("SELECT USERNAME, POINTS FROM CLASSEMENTS ORDER BY POINTS DESC")
+    dbCursor.execute("SELECT USERNAME, POINTS FROM CLASSEMENTS ORDER BY POINTS DESC LIMIT 10")
     res = json.dumps(dbCursor.fetchall())
     dbConnection.close()
     # On doit stocker la reponse avant de fermer la connexion à la base
     return res
 
-def insertClassement(dbFile):
+def insertClassement(dbFile, username, score):
     dbConnection = sqlite3.connect(dbFile)
     dbCursor = dbConnection.cursor()
 
-    dbCursor.execute("INSERT INTO CLASSEMENTS VALUES (?,?, date('now'))", username, points)
+    dbCursor.execute("INSERT INTO CLASSEMENTS VALUES (?,?, date('now'))", (username, score))
     dbConnection.commit()
 
     dbConnection.close()
